9f18f24ac5ae62c638b4abe1744adb7e2869d489
李贤基 <lixianji@163.com>
2019-10-08 14:55:04 +0800

    代码提交

--- src/main/java/com/test/github.java
+++ src/main/java/com/test/github.java
@@ -2,11 +2,6 @@ package com.test;
 
 public class github {
     public static void main(String[] args) {
-        System.out.println("github首次提交代码");
-        System.out.println("github二次提交代码");
-        System.out.println("github三次提交代码");
-        System.out.println("github四次提交代码");
-        System.out.println("github五次提交代码");
         System.out.println("测试名称修改");
     }
 }
